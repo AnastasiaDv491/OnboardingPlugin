b646c92f06d13cc099808a3181a86730a9ef5b45
AnastasiaDv491 <anadvoryanchikova2gmail.com>
2020-11-22 12:07:53 +0100

    taxonomy

--- plugin-onboarding.php
+++ plugin-onboarding.php
@@ -44,10 +44,41 @@ function wpdocs_create_new_taxonomy() {
         'labels'            => $labels,
         'show_ui'           => true,
     );
-    register_taxonomy('flow', array('wp_stages'), $args);
+    register_taxonomy('flow_taxonomy', array('wp_stages'), $args);
 
 }
 add_action('init','wpdocs_create_new_taxonomy', 0);
+
+
+add_shortcode('flow', 'flow_function');
+function flow_function($atts) {
+    $args = array(
+        'post_type' => 'wp_stages',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'flow_taxonomy',
+                'field'    => 'name',
+                'terms'    => $atts['name'],
+            ),
+        ),
+    );
+    $the_query = new WP_Query( $args );
+    if ( $the_query->have_posts() ) {
+        echo "<ul class='onboarding_list' id='onboarding_list_{$atts['name']}'>";
+        while ( $the_query->have_posts() ) {
+            $the_query->the_post();
+            echo '<li>' . get_the_title() . '</li>';
+        }
+        echo '</ul>';
+    } else {
+        // no posts found
+        echo "<h1>No Flows</h1>";
+    }
+    /* Restore original Post Data */
+    wp_reset_postdata();
+}
+
+
 ?>
 
 
